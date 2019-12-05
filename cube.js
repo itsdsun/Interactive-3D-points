706ea10bff763a713a67e8c5de0071a7a22aadb7
Daisy Sun <itsdaisysun@gmail.com>
2019-12-04 18:40:50 -0800

    displayed the points(cubes) into 3d space and implemented orbit control

--- cube.js
+++ cube.js
@@ -1,6 +1,7 @@
 const THREE = require('three');
-const data = require('./Project 2 Data/spiral.json')
-console.log(data);
+const data = require('./Project 2 Data/spiral.json');
+const OrbitControls = require('three-orbitcontrols');
+
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
@@ -8,19 +9,34 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-//CREATING THE CUBE-----------------------------------------------------------------------------------------------
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const movement = new OrbitControls(camera, renderer.domElement);
 
-camera.position.z = 5;
+
+for (let i = 0; i < data.length; i++) {
+console.log(data[i]);
+    
+    //CREATING THE CUBE-----------------------------------------------------------------------------------------------
+    var geometry = new THREE.BoxGeometry( 1, 1, 1);
+    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    var cube = new THREE.Mesh( geometry, material );
+    cube.position.x = data[i].x
+    cube.position.y = data[i].y
+    cube.position.z = data[i].z
+    cube.scale.x = 0.1;
+    cube.scale.y = 0.1;
+    cube.scale.z = 0.1;
+
+    scene.add( cube ); 
+ }
+
+camera.position.z = 50;
+
 
 //RENDERING THE SCENE // ANIMATION -- MOVES THE CUBE -------------------------------------------------------------
 function animate() {
     requestAnimationFrame( animate );
     renderer.render( scene, camera );
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    // cube.rotation.x += 0.01;
+    // cube.rotation.y += 0.01;
 }
 animate();
